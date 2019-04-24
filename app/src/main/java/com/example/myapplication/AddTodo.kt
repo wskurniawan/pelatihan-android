be2263fe68363cffa2c6d8abeb976c08ffe1f08c
wskurniawan <wisnu.kurniawan@mail.ugm.ac.id>
2019-04-25 02:36:32 +0700

    update minggu 2

--- app/src/main/java/com/example/myapplication/AddTodo.kt
+++ app/src/main/java/com/example/myapplication/AddTodo.kt
@@ -1,12 +1,62 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.TimePicker
+import kotlinx.android.synthetic.main.activity_add_todo.*
+import java.util.*
 
 class AddTodo : AppCompatActivity() {
+    private lateinit var selectedDate: Calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
+
+        selectedDate = Calendar.getInstance()
+
+        btnSetDate.setOnClickListener {
+            setDate()
+        }
+
+        btnSetTime.setOnClickListener {
+            setTime()
+        }
+    }
+
+    private  fun setDate(){
+        val calendar = Calendar.getInstance()
+
+        val datepickerDialog = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                selectedDate.set(year, month, dayOfMonth)
+
+                updateSelectedTime()
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+
+        datepickerDialog.show()
+    }
+
+    private fun setTime(){
+        val timePicker = TimePickerDialog(this, object : TimePickerDialog.OnTimeSetListener{
+            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                selectedDate.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                selectedDate.set(Calendar.MINUTE, minute)
+
+                updateSelectedTime()
+            }
+        }, selectedDate.get(Calendar.HOUR_OF_DAY), selectedDate.get(Calendar.MINUTE), true)
+
+        timePicker.show()
+    }
+
+    private fun updateSelectedTime(){
+        val text = "${selectedDate.get(Calendar.DAY_OF_MONTH)} - ${selectedDate.get(Calendar.MONTH) + 1} - ${selectedDate.get(Calendar.YEAR)}, ${selectedDate.get(Calendar.HOUR_OF_DAY)}:${selectedDate.get(Calendar.MINUTE)}"
+
+        txtSelectedDatetime.text = text
     }
 }
--- app/src/main/java/com/example/myapplication/Registerasi.kt
+++ app/src/main/java/com/example/myapplication/Registerasi.kt
@@ -1,12 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_registerasi.*
 
 class Registerasi : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registerasi)
+
+        btnSignup.setOnClickListener(View.OnClickListener {
+            openHome()
+        })
+    }
+
+    private fun openHome(){
+        intent = Intent(this, Home::class.java)
+
+        startActivity(intent)
+        this.finish()
     }
 }
